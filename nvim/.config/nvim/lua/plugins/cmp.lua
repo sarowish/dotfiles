38ca290d228d0167bfca248fbb79e9ec2f0e6e66
Berke Enercan <berkeenercan@tutanota.com>
2025-06-05 19:09:36 +0300

    nvim: add colorful-menu plugin

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -8,65 +8,75 @@ local has_words_before = function()
 end
 
 return {
-    'saghen/blink.cmp',
+    {
+        'saghen/blink.cmp',
 
-    version = '1.*',
+        version = '1.*',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = {
-            preset = 'none',
-            ['<Tab>'] = {
-                function(cmp)
-                    if has_words_before() then
-                        return cmp.select_next()
-                    end
-                end,
-                'snippet_forward',
-                'fallback',
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            keymap = {
+                preset = 'none',
+                ['<Tab>'] = {
+                    function(cmp)
+                        if has_words_before() then
+                            return cmp.select_next()
+                        end
+                    end,
+                    'snippet_forward',
+                    'fallback',
+                },
+                ['<S-Tab>'] = { 'select_prev', "snippet_backward" },
+                ['<CR>'] = { 'accept', 'fallback' },
+                ['<C-u>'] = { 'scroll_documentation_up' },
+                ['<C-d>'] = { 'scroll_documentation_down' },
+                ['<C-space>'] = { 'show', 'hide_documentation', 'show_documentation' },
+                ['<C-e>'] = { 'hide' },
+                ['<Esc>'] = { 'cancel', 'fallback' },
             },
-            ['<S-Tab>'] = { 'select_prev', "snippet_backward" },
-            ['<CR>'] = { 'accept', 'fallback' },
-            ['<C-u>'] = { 'scroll_documentation_up' },
-            ['<C-d>'] = { 'scroll_documentation_down' },
-            ['<C-space>'] = { 'show', 'hide_documentation', 'show_documentation' },
-            ['<C-e>'] = { 'hide' },
-            ['<Esc>'] = { 'cancel', 'fallback' },
-        },
 
-        appearance = {
-            nerd_font_variant = 'mono'
-        },
-
-        completion = {
-            documentation = {
-                auto_show = true,
-                auto_show_delay_ms = 50,
-                window = {
-                    max_height = 40,
-                    border = 'rounded'
-                }
+            appearance = {
+                nerd_font_variant = 'mono'
             },
-            list = { selection = { preselect = false } },
-            ghost_text = { enabled = true },
-            menu = {
-                border = 'rounded',
-                draw = {
-                    columns = { { 'kind_icon' }, { 'label' }, { 'kind' }, { 'label_description' } },
-                    components = {
-                        label_description = { width = { max = 40 } }
+
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 50,
+                    window = {
+                        max_height = 40,
+                        border = 'rounded'
+                    }
+                },
+                list = { selection = { preselect = false } },
+                ghost_text = { enabled = true },
+                menu = {
+                    border = 'rounded',
+                    draw = {
+                        columns = { { 'kind_icon' }, { 'label' }, { 'kind' }, { 'label_description' } },
+                        components = {
+                            label = {
+                                text = function(ctx)
+                                    return require("colorful-menu").blink_components_text(ctx)
+                                end,
+                                highlight = function(ctx)
+                                    return require("colorful-menu").blink_components_highlight(ctx)
+                                end,
+                            },
+                        },
                     }
                 }
-            }
+            },
+            snippets = { preset = "luasnip" },
+            sources = {
+                default = { 'lsp', 'path', 'snippets', 'buffer' },
+            },
+            fuzzy = {
+                implementation = "prefer_rust_with_warning"
+            },
         },
-        snippets = { preset = "luasnip" },
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
-        fuzzy = {
-            implementation = "prefer_rust_with_warning"
-        },
+        opts_extend = { "sources.default" }
     },
-    opts_extend = { "sources.default" }
+    { 'xzbdmw/colorful-menu.nvim' }
 }
